198478607c1a3ed5d2c2551959809b428fec0efe
Flothaboss1000 <flothaboss1000@gmail.com>
2022-06-24 20:26:13 +0530

    Made countdown timer functional

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,5 +1,56 @@
 import React from "react";
 
 export default function Timer() {
-  return <div className="taskbar--timer">T - 00d:00h:00m:00s</div>;
+  let entry = {
+    dd: 0,
+    hh: 0,
+    mm: 0,
+    ss: 0,
+    sign: "-",
+  };
+  const [countdown, setCountdown] = React.useState(entry);
+
+  React.useEffect(() => {
+    updateTimer();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [countdown]);
+
+  function updateTimer() {
+    setTimeout(() => {
+      const target = new Date("Aug 6, 2022 00:00:00").getTime();
+      const time = new Date().getTime();
+      const diff = target - time > 0 ? target - time : time - target;
+      let newsign = "-";
+      if (target - time < 0) newsign = "+";
+
+      var newdd = Math.floor(diff / (1000 * 60 * 60 * 24));
+      var newhh = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var newmm = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+      var newss = Math.floor((diff % (1000 * 60)) / 1000);
+
+      let countdown = {
+        dd: newdd,
+        hh: newhh,
+        mm: newmm,
+        ss: newss,
+        sign: newsign,
+      };
+
+      setCountdown(countdown);
+      console.log(diff);
+    }, 1000);
+  }
+  function addDoubleZero(num) {
+    if (num < 10) {
+      return `0${String(num)}`;
+    } else return num;
+  }
+
+  return (
+    <div className="taskbar--timer">
+      T {countdown.sign} {addDoubleZero(countdown.dd)}dd:{" "}
+      {addDoubleZero(countdown.hh)}hh: {addDoubleZero(countdown.mm)}mm:{" "}
+      {addDoubleZero(countdown.ss)}ss
+    </div>
+  );
 }

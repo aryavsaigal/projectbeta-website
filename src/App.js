0f0df466d9d0504247acf266456babf64c709f51
Flothaboss1000 <flothaboss1000@gmail.com>
2022-06-24 16:56:47 +0530

    Title screen added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Taskbar from "./components/Taskbar";
 import Desktop from "./components/Desktop";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
     <div className="App">
       <Desktop />
       <Taskbar />
+      <Homepage />
     </div>
   );
 }

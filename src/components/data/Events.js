ac79e9685496b2c9c75b3c56366843b40a2ff02a
SuyashC1000 <8592@sanskritischool.edu.in>
2022-06-26 21:14:12 +0530

    UI Colour Modifications

---
+++ src/components/data/Events.js
@@ -0,0 +1,18 @@
+import sample_src from "../../images/events/sample.png";
+
+export default function EventsFolder() {
+  const eventsData = [
+    { name: "Hypertext{}", img_src: sample_src },
+    { name: "Function Overload", img_src: sample_src },
+    { name: "Designathon", img_src: sample_src },
+    { name: "Dual Wield", img_src: sample_src },
+    { name: "Side Quest", img_src: sample_src },
+    { name: "GD", img_src: sample_src },
+    { name: "Audio Engineering", img_src: sample_src },
+    { name: "AI/ML Workshop", img_src: sample_src },
+    { name: "BetaTest", img_src: sample_src },
+    { name: "64 Wit", img_src: sample_src },
+  ];
+
+  return eventsData;
+}

51d6250fe9521b8f5143fd0122f2f876338767e2
SuyashC1000 <8592@sanskritischool.edu.in>
2022-06-28 15:16:24 +0530

    Partially updated team data

--- src/components/data/teamData.js
+++ src/components/data/teamData.js
@@ -1,66 +1,212 @@
 import stock_photo from "../../images/team/stock-photo.jpg";
 
-export default function EventsFolder() {
+import aadi_jain from "../../images/team/aadi_jain.jpeg";
+import adit_magotra from "../../images/team/adit_magotra.jpeg";
+import aniket_kumar from "../../images/team/aniket_kumar.jpeg";
+import arnav_whig from "../../images/team/arnav_whig.jpeg";
+import chirag_sehgal from "../../images/team/chirag_sehgal.jpeg";
+import dhimant_nath from "../../images/team/dhimant_nath.jpeg";
+import gauri_tandon from "../../images/team/gauri_tandon.jpeg";
+import gayatri from "../../images/team/gayatri.jpeg";
+import kanha_singh from "../../images/team/kanha_singh.jpeg";
+import navvye_anand from "../../images/team/navvye_anand.png";
+import puranjay_chawla from "../../images/team/puranjay_chawla.jpeg";
+import rishi_mathur from "../../images/team/rishi_mathur.jpeg";
+import shreeya_mishra from "../../images/team/shreeya_mishra.jpeg";
+import simar_tandon from "../../images/team/simar_tandon.jpeg";
+import taran_singh from "../../images/team/taran_singh.jpeg";
+
+export default function teamData() {
   return {
     board: [
       {
         name: "Simar Tandon",
         hod: "Group Discussion",
-        img_src: stock_photo,
-        quote: "This is a sample quote.",
+        img_src: simar_tandon,
+        quote: "men and eb garamond- mid as hell",
         title: "President",
       },
       {
         name: "Aadi Jain",
         hod: "Web Development",
-        img_src: stock_photo,
-        quote: "This is a sample quote.",
+        img_src: aadi_jain,
+        quote: "The fallen leaves tell a story",
         title: "Vice President",
       },
       {
         name: "Adit Magotra",
         hod: "Programming",
-        img_src: stock_photo,
+        img_src: adit_magotra,
         quote: "This is a sample quote.",
         title: "Secretary",
       },
     ],
     core: [
       {
-        name: "John Doe",
-        hod: "Department",
+        name: "Navvye Anand",
+        hod: "Web Development",
+        img_src: navvye_anand,
+        quote: "This is a sample quote.",
+      },
+      {
+        name: "Kanha Singh",
+        hod: "Programming",
+        img_src: kanha_singh,
+        quote: "This is a sample quote.",
+      },
+      {
+        name: "Kabir Verdi Jain",
+        hod: "Logistics",
         img_src: stock_photo,
         quote: "This is a sample quote.",
       },
       {
-        name: "John Doe",
-        hod: "Department",
+        name: "Vidushi Jain",
+        hod: "Logistics",
         img_src: stock_photo,
         quote: "This is a sample quote.",
       },
       {
-        name: "John Doe",
-        hod: "Department",
+        name: "Shreeya Mehra",
+        hod: "Quizzing",
+        img_src: shreeya_mishra,
+        quote: "This is a sample quote.",
+      },
+      {
+        name: "Inika Harikrishnan",
+        hod: "Quizzing",
         img_src: stock_photo,
         quote: "This is a sample quote.",
       },
       {
-        name: "John Doe",
-        hod: "Department",
+        name: "Gayatri",
+        hod: "Graphic Design",
+        img_src: gayatri,
+        quote: "This is a sample quote.",
+      },
+      {
+        name: "Gauri Tandon",
+        hod: "Graphic Design",
+        img_src: gauri_tandon,
+        quote: "I feel like the letter k in the word knife",
+      },
+      {
+        name: "Paavan Gupta",
+        hod: "Group Discussion",
         img_src: stock_photo,
         quote: "This is a sample quote.",
       },
-    ],
+      {
+        name: "Aryav Saigal",
+        hod: "AI/IoT Robotics",
+        img_src: stock_photo,
+        quote: "This is a sample quote.",
+      },
+      {
+        name: "Shreyas Vertia",
+        hod: "AI/IoT Robotics",
+        img_src: stock_photo,
+        quote: "This is a sample quote.",
+      },
+      {
+        name: "Dhimant Nath",
+        hod: "PC Gaming",
+        img_src: dhimant_nath,
+        quote: "This is a sample quote.",
+      },
+      {
+        name: "Taran Singh",
+        hod: "PC Gaming",
+        img_src: taran_singh,
+        quote: "This is a sample quote.",
+      },
+      {
+        name: "Rishi Mathur",
+        hod: "Console Gaming & Cryptic Hunt",
+        img_src: rishi_mathur,
+        quote: "This is a sample quote.",
+      },
+      {
+        name: "Gerick Razdan",
+        hod: "Console Gaming",
+        img_src: stock_photo,
+        quote: "This is a sample quote.",
+      },
+      {
+        name: "Chirag Sehgal",
+        hod: "A/V Editing & Cryptic Hunt",
+        img_src: chirag_sehgal,
+        quote: "This is a sample quote.",
+      },
+      {
+        name: "Aniket Kumar",
+        hod: "A/V Editing",
+        img_src: aniket_kumar,
+        quote: "chinta walli baat hi nahi hai",
+      },
+      {
+        name: "Puranjay Chawla",
+        hod: "Pitching",
+        img_src: puranjay_chawla,
+        quote: "This is a sample quote.",
+      },
+      {
+        name: "Sia Jha Nath",
+        hod: "Pitching",
+        img_src: stock_photo,
+        quote: "This is a sample quote.",
+      },
+      {
+        name: "Arnav Whig",
+        hod: "Audio Engineering",
+        img_src: arnav_whig,
+        quote: "This is a sample quote.",
+      },
+    ].sort((a, b) => a.name.localeCompare(b.name)),
     members: [
-      "John Doe",
-      "John Doe",
-      "John Doe",
-      "John Doe",
-      "John Doe",
-      "John Doe",
-      "John Doe",
-      "John Doe",
-      "John Doe",
-    ],
+      "Aayush Mahapatra",
+      "Arbor",
+      "Arjun Singhal",
+      "Aarav Bansal",
+      "Shrey Mehra",
+      "Medha Shrivastava ",
+      "Vandan Parakh",
+      "Samarveer Singh Tuli",
+      "Naphirisa Nongrang",
+      "Dwij Mishra",
+      "Kanav Goyal",
+      "Shaurya Rastogi",
+      "Hardik Khurana",
+      "Ved Gautam",
+      "Aadya Narula",
+      "Vinayak Navneet Kothari ",
+      "Ishmeet Wadhwa",
+      "Seemon Saraogi",
+      "Reyhaan Aryan",
+      "Azaan Karan",
+      "Aarya Sharma",
+      "Naiwrit Joy Shukla",
+      "Tvisha Tiwari",
+      "Arjun Taneja ",
+      "Shikraj Singh",
+      "Abhijay Kansal",
+      "Shaurya Midha",
+      "Arnav Jain",
+      "Shaurya JM",
+      "Vihaan Pathak",
+      "Ananmay Jalan Wadhwa",
+      "Amay Mangal",
+      "Suyash Chandel",
+      "Angelina Deol",
+      "Ujaan Sen",
+      "Anaahat Singh ",
+      "Naina Singh",
+      "Yugyal",
+      "Jaskirat Singh Maskeen",
+      "Syed Ali Akbar",
+      "Kartik Singh",
+      "Anvee Choudary",
+      "Kashvi Pathak",
+    ].sort(),
   };
 }

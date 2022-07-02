0b0cc8c7fff3ed3a2376c6cbd4ab5eb03f561b1a
SuyashC1000 <8592@sanskritischool.edu.in>
2022-07-02 21:22:15 +0530

    Test for perf. imporvement in mobile BG 2

--- src/components/home/Background.js
+++ src/components/home/Background.js
@@ -8,21 +8,41 @@ export default function Background() {
   const obj = React.useRef(null);
   React.useEffect(() => {
     if (!vantaEffect) {
-      setVantaEffect(
-        NET({
-          el: obj.current,
-          mouseControls: true,
-          touchControls: false,
-          gyroControls: false,
-          minHeight: 200.0,
-          minWidth: 200.0,
-          scale: 1.0,
-          scaleMobile: 4.0,
-          backgroundColor: 0x1a293e,
-          color: 0x813f22,
-          showDots: false,
-        })
-      );
+      if (window.innerWidth > 900)
+        setVantaEffect(
+          NET({
+            el: obj.current,
+            mouseControls: true,
+            touchControls: false,
+            gyroControls: false,
+            minHeight: 200.0,
+            minWidth: 200.0,
+            scale: 1.0,
+            scaleMobile: 1.0,
+            backgroundColor: 0x1a293e,
+            color: 0x813f22,
+            showDots: false,
+          })
+        );
+      else if (window.innerWidth <= 900)
+        setVantaEffect(
+          NET({
+            el: obj.current,
+            mouseControls: false,
+            touchControls: false,
+            gyroControls: false,
+            minHeight: 500.0,
+            minWidth: 200.0,
+            scale: 1.0,
+            scaleMobile: 2.0,
+            backgroundColor: 0x1a293e,
+            color: 0x813f22,
+            showDots: false,
+            points: 4.0,
+            maxDistance: 25.0,
+            spacing: 30.0,
+          })
+        );
     }
     return () => {
       if (vantaEffect) vantaEffect.destroy();

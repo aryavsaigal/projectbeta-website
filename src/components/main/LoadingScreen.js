e74351fd42de6525b3e5ffb038f24f47717ff277
SuyashC1000 <8592@sanskritischool.edu.in>
2022-07-03 17:58:49 +0530

    Ensured loading screen exits after animation ends

--- src/components/main/LoadingScreen.js
+++ src/components/main/LoadingScreen.js
@@ -7,25 +7,29 @@ export default function LoadingScreen() {
   const obj_child = React.useRef();
   React.useLayoutEffect(() => {
     obj_child.current.style.display = "block";
-    let wait = setTimeout(() => {
-      obj_parent.current.style.opacity = "0";
-      obj_child.current.style.display = "none";
-    }, 2500);
-    let wait2 = setTimeout(() => {
-      obj_parent.current.style.display = "none";
-    }, 3500);
+  }, []);
 
+  function handleEnd() {
+    obj_parent.current.style.opacity = "0";
+    obj_child.current.style.display = "none";
+    let wait = setTimeout(() => {
+      obj_parent.current.style.display = "none";
+    }, 500);
     return () => {
       clearTimeout(wait);
-      clearTimeout(wait2);
     };
-  }, []);
+  }
 
   return (
     <div className="loadingscreen" ref={obj_parent}>
-      <video ref={obj_child} autoPlay muted>
-        <source src={pbbootup_src} type="video/mp4" />
-      </video>
+      <video
+        ref={obj_child}
+        src={pbbootup_src}
+        type="video/mp4"
+        onEnded={handleEnd}
+        autoPlay
+        muted
+      ></video>
     </div>
   );
 }

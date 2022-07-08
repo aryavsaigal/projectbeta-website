635e0f2aa2c9304592c922b5de189839dd49c765
SuyashC1000 <8592@sanskritischool.edu.in>
2022-07-08 18:27:25 +0530

    Updated teams page + directory

---
+++ src/components/data/events/ItsDebatable.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+export default function ItsDebatable() {
+  return (
+    <>
+      <ul>
+        <li>
+          <p>It is an individual event.</p>
+        </li>
+        <li>
+          <p>
+            There will be a preliminary as well as a final round for the same.
+          </p>
+        </li>
+      </ul>
+      <h3>Preliminary Round :</h3>
+      <ul>
+        <li>
+          <p>
+            Participants are required to submit a video of themselves speaking
+            either for or against the motion.
+          </p>
+        </li>
+        <li>
+          <p>
+            The duration of the video must not exceed 120 seconds, and the
+            participant should be clearly audible and visible.
+          </p>
+        </li>
+        <li>
+          <p>
+            Plagiarism of any kind in the participant's speech will lead to
+            immediate disqualification.
+          </p>
+        </li>
+        <li>
+          <p>
+            Participants are required to submit their video in the form of a
+            drive link by 10th August using the given form:
+            <br />
+            <a href="https://forms.gle/89fmeJ6zgev51RGcA">
+              https://forms.gle/89fmeJ6zgev51RGcA
+            </a>
+          </p>
+        </li>
+      </ul>
+      <h3>Final Round:</h3>
+      <ul>
+        <li>
+          <p>
+            Topic for the final round will be released on the spot. Participants
+            will be given 30 minutes to prepare.
+          </p>
+        </li>
+        <li>
+          <p>Participants will be required to give:</p>
+          <ul>
+            <li>
+              <p>30-45 seconds opening statement</p>
+            </li>
+            <li>
+              <p>90-120 seconds of main content</p>
+            </li>
+            <li>
+              <p>30-45 seconds closing statement</p>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <p>All participants will be questioned by the judges.</p>
+        </li>
+        <li>
+          <p>
+            Interjections from the participants are not compulsory but are
+            preferred.
+          </p>
+        </li>
+      </ul>
+    </>
+  );
+}
---
+++ src/components/data/events/SideQuest.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function SideQuest() {
+  return (
+    <>
+      <h2>Valorant</h2>
+      <ul>
+        <li>
+          <p>5v5 on valorant</p>
+        </li>
+        <li>
+          <p>Teams will be formed on the spot</p>
+        </li>
+        <li>
+          <p>No restrictions on agent selection</p>
+        </li>
+        <li>
+          <p>Regular competitive rules</p>
+        </li>
+        <li>
+          <p>For map selection a veto system will be done</p>
+        </li>
+        <li>
+          <p>Deathmatches will also be held</p>
+        </li>
+      </ul>
+    </>
+  );
+}
